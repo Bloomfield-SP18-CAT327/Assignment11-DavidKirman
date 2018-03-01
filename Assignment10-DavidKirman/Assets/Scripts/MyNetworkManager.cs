eb550d1530086d6e20a1a1817c6cd66a0888c87a
kirmanator <davidsk95@yahoo.com>
2018-03-01 18:54:08 -0500

    Network manager
    Added player
    Added enemy
    Added my own network manager script
    set player's color to new color

---
+++ Assignment10-DavidKirman/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public override void OnServerAddPlayer (NetworkConnection conn, short playerControllerId)
+	{
+		GameObject playerToSpawn = (GameObject) Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+		playerToSpawn.GetComponent<Player> ().color = new Color (Random.Range (0, 1), Random.Range (0, 1), Random.Range (0, 1));
+		NetworkServer.AddPlayerForConnection (conn, playerToSpawn, playerControllerId);
+	}
+}
